0453a80cab02bec1c33ae309cc97dadf87d0dcd7
Rawal <rawalsandhan2909@gmail.com>
2025-12-04 16:18:26 -0500

    Deploying Lambda and creating image

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,21 +1,4 @@
-# infrastructure/variables.tf
-
-variable "project_name" {
-  description = "Name for the OpenShift project/namespace"
-  default     = "blockchain-verifier"
-}
-
 variable "image_tag" {
-  description = "Docker image tag for the verifier application"
-  default     = "latest"
-}
-
-variable "contract_address" {
-  description = "The deployed Ethereum smart contract address"
-  type        = string
-}
-
-variable "rpc_url" {
-  description = "Ethereum Node RPC URL (Sepolia/Mainnet)"
+  description = "The docker image tag to deploy"
   type        = string
 }
