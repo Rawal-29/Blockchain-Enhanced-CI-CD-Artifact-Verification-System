01542c3df29882572d26b1eecb58b23bcf8d53af
Rawal <rawalsandhan2909@gmail.com>
2025-12-10 13:02:12 -0500

    infra

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,2 +1,10 @@
-variable "image_tag" { type = string }
-variable "contract_address" { type = string; default = "0x0000000000000000000000000000000000000000" }
+variable "image_tag" {
+  description = "The docker image tag to deploy"
+  type        = string
+}
+
+variable "contract_address" {
+  description = "The deployed smart contract address"
+  type        = string
+  default     = "0x0000000000000000000000000000000000000000"
+}
